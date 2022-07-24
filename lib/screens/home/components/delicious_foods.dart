19bcce3f17fbe4bd3c3bdbc314e3d3bb0dd42aee
garv sharma <sgarv0710@gmail.com>
2022-07-24 10:22:15 +0530

    Food app UI flutter

--- lib/screens/home/components/delicious_foods.dart
+++ lib/screens/home/components/delicious_foods.dart
@@ -13,11 +13,27 @@ class DeliciousFoods extends StatelessWidget {
       child: Row(
         children: [
           DeliciousFoodsCard(
+<<<<<<< HEAD
             image: "assets/images/NO.png",
             press: () {},
           ),
           DeliciousFoodsCard(
             image: "assets/images/pas.png",
+=======
+            image: "assets/images/doooo.png",
+            press: () {},
+          ),
+          DeliciousFoodsCard(
+            image: "assets/images/la.png",
+            press: () {},
+          ),
+          DeliciousFoodsCard(
+            image: "assets/images/download.png",
+            press: () {},
+          ),
+          DeliciousFoodsCard(
+            image: "assets/images/download (1).png",
+>>>>>>> 97c11b6 (Food app UI flutter)
             press: () {},
           ),
         ],
@@ -41,10 +57,17 @@ class DeliciousFoodsCard extends StatelessWidget {
       child: Container(
         margin: EdgeInsets.only(
             left: kDefaultPadding,
+<<<<<<< HEAD
             top: kDefaultPadding / 2,
             bottom: kDefaultPadding / 2),
         width: size.width * 0.8,
         height: 185,
+=======
+            top: kDefaultPadding ,
+            bottom: kDefaultPadding ),
+        width: size.width * 0.38,
+        height: 160,
+>>>>>>> 97c11b6 (Food app UI flutter)
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             image:
